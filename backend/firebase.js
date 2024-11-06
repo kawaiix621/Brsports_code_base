b5b72c5365a01d739f77b344a2fe50522dbada9d
tobi-awolaju (kawaii) <120692959+kawaiix621@users.noreply.github.com>
2024-11-06 01:45:06 +0100

    Update firebase.js

--- backend/firebase.js
+++ backend/firebase.js
@@ -1,5 +1,6 @@
 const admin = require("firebase-admin");
-const serviceAccount = require("./config/serviceAccountKey.json");
+const serviceAccount = JSON.parse(process.env.GOOGLE_CREDENTIALS);
+
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
