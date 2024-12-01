e9b51bfeacee813cf7d77968ab20a1e5a0df6d68
tobi-awolaju (kawaii) <120692959+kawaiix621@users.noreply.github.com>
2024-12-01 20:36:50 +0100

    :O

--- frontend/src/pages/SignIn.js
+++ frontend/src/pages/SignIn.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { signInWithGoogle, signInWithTwitter, db, ref, get, set } from "../firebase"; 
-import "./SignIn.css";
+
 
 const SignIn = () => {
   const navigate = useNavigate();
