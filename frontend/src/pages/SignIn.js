3029f384f38cad348f647ef86ff180db89dc5c6f
tobi-awolaju (kawaii) <120692959+kawaiix621@users.noreply.github.com>
2024-12-03 02:30:20 +0100

    :)

--- frontend/src/pages/SignIn.js
+++ frontend/src/pages/SignIn.js
@@ -48,38 +48,62 @@ const SignIn = () => {
 
   return (
 <div className="signin-container">
-<div style={{ height: '100px' }}></div>
-<h1 style={{
-  fontSize:"60px",
-  margin:"0px",
-  fontWeight:"normal",
-  fontFamily:"Bagel",
-  color:"black"
-}}>
-BRsports
-</h1>
-<p style={{
-  fontSize:"20px",
-  margin:"4px",
-color:"grey"
-}}>We don't match couples we match rivals</p>
+<div style={{ height: '70px' }}></div>
+
+<img style={{
+  width:"100vw",
+  height:"auto",
+  borderRadius:"0px"
+}}
+src="banner.png">
+</img>
+
+<div className="header"> {/*Use className for styling*/}
+        <h1 className="title">Welcome to BRsports</h1>
+        <p className="subtitle">The most acessible Esport Platform</p>
+      </div>
+
+      <div className="about"> {/*Section for About Us*/}
+        <h2>A new way to Play Your Games</h2>
+        <p>
+        Esports.gg was launched by a diverse group of esports
+         industry veterans to finally give esports, competitive 
+         gaming and streaming the dedicated home it deserves. With 
+         contributions from peak-of-form professional athletes, singular 
+         </p> {/* Your about us text here */}
+
+        <h2>A fair Ranking System</h2>
+        <p>Esports.gg is a destination connecting 
+          esports fans to their passions. We create 
+          news and analysis for all gamers, esports 
+</p> {/*Your Mission text */}
 
 
-
-
+<h2>Understand BRsports</h2>
+        <p> 
+          Monad Labs is building the next-generation b
+          lockchain infrastructure for Monad,
+           a 100% EVM-compatible layer 1 blockchain boasting
+            10,000 transactions per second, 1-second block times, 
+            single-slot finality, and low-hardware requirements.
+</p> {/*Your Mission text */}
+</div>
 <div className="signin">
         <button style={{
           opacity:"1"
         }} type="button" onClick={() => handleSignIn(signInWithGoogle)}>
-        Signup
+        Gammer's Portal
         </button>
-        <button  style={{
+      {/*  <button  style={{
           opacity:"1"
         }} type="button" onClick={() => handleSignIn(signInWithTwitter)}>
-        Login
-        </button>
-      </div>
-<div style={{ height: '400px' }}></div>
+        Login with X
+        </button> */}
+</div>
+
+{/*Your Mission text */}
+
+<div style={{ height: '100px' }}></div>
 </div>
   );
 };
